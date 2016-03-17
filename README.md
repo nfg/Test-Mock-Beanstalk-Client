18b2d35234c9bfe56c4b5ed605e807e145ef9701
Nigel Gregoire <nigelgregoire@gmail.com>
2016-03-16 17:09:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Test::Mock::Beanstalk::Client - Blah blah blah
+
+# SYNOPSIS
+
+    use Test::Mock::Beanstalk::Client;
+
+# DESCRIPTION
+
+Test::Mock::Beanstalk::Client is
+
+# AUTHOR
+
+Nigel Gregoire &lt;nigelgregoire@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2016- Nigel Gregoire
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Test/Mock/Beanstalk/Client.pm
@@ -0,0 +1,39 @@
+package Test::Mock::Beanstalk::Client;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::Mock::Beanstalk::Client - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Test::Mock::Beanstalk::Client;
+
+=head1 DESCRIPTION
+
+Test::Mock::Beanstalk::Client is
+
+=head1 AUTHOR
+
+Nigel Gregoire E<lt>nigelgregoire@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2016- Nigel Gregoire
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
